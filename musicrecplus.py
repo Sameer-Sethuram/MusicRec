72cfd81164cfc72ae88c8c64f8b8dd4fcb6a8456
Elianfer <150724542+Elianfer@users.noreply.github.com>
2023-11-16 16:13:58 -0500

    Elian

--- musicrecplus.py
+++ musicrecplus.py
@@ -1,27 +1,29 @@
 import os
+import os
+
+pref_file = 'musicrecplus.txt'
 
 def file_verf():
     #Elian F
     if os.path.exists("musicrecplus.txt"):
-        print("DataBase Found!")
+        print("Database Found!")
     else:
-        f = open("musicrecplus.txt", "a+") 
-        print('New file named musicrecplus.txt created!')
-pref_file = 'musicrecplus.txt'
+        with open("musicrecplus.txt", "a+") as f:
+            print('New file named musicrecplus.txt created!')
 
-    
 def load_users(file):
-    #Elian F
-    file = open('musicrecplus.txt', 'r+')
+    #Elian F   
     users = {}
-
-    for line in file:
-        user, band = line.strip().split(":")
-        bands = band.split(",")
-        bands.sort()
-        users[user] = bands
+    with open(file, 'r+') as file:
+        for line in file:
+            user, band = line.strip().split(":")
+            bands = band.split(",")
+            bands.sort()
+            users[user] = bands
     return users
+
 def get_pref(username, users):
+    #Elian F
     if username in users:
         pref = users[username]
         print(f"Welcome back to musicrecplus, {username}!")
@@ -37,54 +39,51 @@ def get_pref(username, users):
             prefs.append(new_pref.strip().title())
 
         users[username] = prefs
+        print("Your preferences have been saved!")
     return users
-load_users(pref_file)
-        
 
-def get_rec(current_user, prefs,users):
-    pass
+def get_rec(current_user, prefs, users):
+    pass  # Define how to get recommendations
 
-def match_best_users(cuurrent_user, prefs, users):
-    pass
-
+def match_best_users(current_user, prefs, users):
+    pass  # Define how to match best users
 
 def save_pref(username, prefs, users, file):
-    pass
-
+    with open(file, 'w') as f:
+        for user, bands in users.items():
+            bands_str = ",".join(bands)
+            f.write(f"{user}:{bands_str}\n")
 
 def get_menu():
-
-    choice = input("""
-    Enter a letter to choose an option: 
-    e - Enter preferences
-    r - Get recommendations
-    p - Show most popular artists
-    h - How popular is the most popular 
-    m - Which user has the most likes
-    q - Save and quit
-    >>""")
-    if choice == 'r':
-        users = load_users(pref_file)
-        current_user = input("Please Enter Your Username: ")
-        prefs = get_pref(current_user, users)
-        get_rec(current_user, prefs, users)
-    elif choice == 'p':
-        pass
-    elif choice == 'h':
-        pass
-    elif choice =='m':
-        pass
-    elif choice == 'q':
-            save_pref(current_user, prefs, users, pref_file)
+    load_users(pref_file)
+    while True:
+        choice = input("""
+        Enter a letter to choose an option: 
+        e - Enter preferences
+        r - Get recommendations
+        p - Show most popular artists
+        h - How popular is the most popular 
+        m - Which user has the most likes
+        q - Save and quit
+        >>""")
         
-        
-print(get_menu())
-
-def main():
-    file_verf()
-    users = load_users(pref_file)
-    print("Welcome To Music Recommendator")
-
-    username = input("Please Enter Your Username: ")
-    print(f"Welcome {username} to Music Recommendator")
-    
+        if choice == 'e':
+            username = input("Enter your username: ")
+            users = load_users(pref_file)
+            get_pref(username, users)
+        elif choice == 'r':
+            # Assuming you pass current_user, prefs, and users to get recommendations
+            
+            get_rec(current_user, prefs, users)
+        elif choice == 'p':
+            # Show most popular artists 
+            pass
+        elif choice == 'h':
+            # How popular 
+            pass
+        elif choice == 'm':
+            # Which user has the most likes 
+            pass
+        elif choice == 'q':
+            save_pref(username, prefs, users, pref_file)
+            break
