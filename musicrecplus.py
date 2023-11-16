9eff7888799bb6f31db4b0e1ac65f00aae7f7f5e
Elianfer <150724542+Elianfer@users.noreply.github.com>
2023-11-16 15:51:42 -0500

    New attempt

--- musicrecplus.py
+++ musicrecplus.py
@@ -9,6 +9,7 @@ def file_verf():
         print('New file named musicrecplus.txt created!')
 pref_file = 'musicrecplus.txt'
 
+    
 def load_users(file):
     file = open('musicrecplus.txt', 'r+')
     users = {}
@@ -19,11 +20,21 @@ def load_users(file):
         bands.sort()
         users[user] = bands
     return users
-file_verf()
-print(load_users("musicrecplus.txt"))
-
 def get_pref(username, users):
-    pass
+    new_pref = ''
+    if username in users:
+        pref = users[username]
+        print(f"Welcome back to musicrecplus {username}!")
+        get_menu(current_user=username, pref=pref, users=users)
+    else:
+        prefs =[]
+        print(f"Welcome to musicrecplus {username}!")
+        new_pref = input("Please Enter Your Artist/Bands: ")
+        while new_pref != '':
+            prefs.append(new_pref.strip().title())
+            print("Please Enter Another Artist/Bands: ")
+
+        
 
 def get_rec(current_user, prefs,users):
     pass
@@ -31,14 +42,37 @@ def get_rec(current_user, prefs,users):
 def match_best_users(cuurrent_user, prefs, users):
     pass
 
-def drop(l1, l2):
-    pass
-
-def numMatch(l1, l2):
-    pass
 
 def save_pref(username, prefs, users, file):
     pass
+
+
+def get_menu():
+    users = load_users(pref_file)
+    current_user = input("Please Enter Your Username: ")
+    prefs = get_pref(current_user, users)
+    choice = input("""
+    Enter a letter to choose an option: 
+    e - Enter preferences
+    r - Get recommendations
+    p - Show most popular artists
+    h - How popular is the most popular 
+    m - Which user has the most likes
+    q - Save and quit
+    >>""")
+    if choice == 'r':
+        get_rec(current_user, prefs, users)
+    elif choice == 'p':
+        pass
+    elif choice == 'h':
+        pass
+    elif choice =='m':
+        pass
+    elif choice == 'q':
+            save_pref(current_user, prefs, users, pref_file)
+        
+        
+print(get_menu())
 
 def main():
     file_verf()
@@ -47,4 +81,4 @@ def main():
 
     username = input("Please Enter Your Username: ")
     print(f"Welcome {username} to Music Recommendator")
-    pref = get_pref(username, users)
+    
